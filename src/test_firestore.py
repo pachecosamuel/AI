56571d22194af7eabd0d64eb86b3c7f9fa21513e
pachecosamuel <pacheco.samuel@hotmail.com>
2025-02-23 12:17:00 -0300

    conexão básica ao firebase implementada

---
+++ src/test_firestore.py
@@ -0,0 +1,11 @@
+from utils.firebase import db
+
+def test_firestore():
+    users_ref = db.collection("users")
+    docs = users_ref.stream()
+
+    for doc in docs:
+        print(f"{doc.id} => {doc.to_dict()}")
+
+if __name__ == "__main__":
+    test_firestore()
